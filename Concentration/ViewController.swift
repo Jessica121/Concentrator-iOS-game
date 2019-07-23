1ececf6b03e8dbee5b2cefc7648b5450bbc55740
Rongrong Wang <janesis@hotmail.com>
2019-07-23 11:54:45 -0700

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Rongrong Wang on 2019/7/23.
+//  Copyright © 2019 Rongrong Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
